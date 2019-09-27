ac3fb7ea54bfdbd6f19829c05d6978564fea7e11
stephsalou <49046464+stephsalou@users.noreply.github.com>
2019-09-27 15:00:00 +0000

    second

--- scrap.py
+++ scrap.py
@@ -35,3 +35,52 @@ def visitor_ip_address(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+pip install pyyaml ua-parser user-agents
+pip install django-user-agents
+
+INSTALLED_APPS = (
+    # Other apps...
+    'django_user_agents',
+)
+
+# Cache backend is optional, but recommended to speed up user agent parsing
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+# Name of cache backend to cache user agents. If it not specified default
+# cache alias will be used. Set to `None` to disable caching.
+USER_AGENTS_CACHE = 'default'
+
+MIDDLEWARE_CLASSES = (
+    # other middlewares...
+    'django_user_agents.middleware.UserAgentMiddleware',
+)
+def my_view(request):
+
+    # Let's assume that the visitor uses an iPhone...
+    request.user_agent.is_mobile # returns True
+    request.user_agent.is_tablet # returns False
+    request.user_agent.is_touch_capable # returns True
+    request.user_agent.is_pc # returns False
+    request.user_agent.is_bot # returns False
+
+    # Accessing user agent's browser attributes
+    request.user_agent.browser  # returns Browser(family=u'Mobile Safari', version=(5, 1), version_string='5.1')
+    request.user_agent.browser.family  # returns 'Mobile Safari'
+    request.user_agent.browser.version  # returns (5, 1)
+    request.user_agent.browser.version_string   # returns '5.1'
+
+    # Operating System properties
+    request.user_agent.os  # returns OperatingSystem(family=u'iOS', version=(5, 1), version_string='5.1')
+    request.user_agent.os.family  # returns 'iOS'
+    request.user_agent.os.version  # returns (5, 1)
+    request.user_agent.os.version_string  # returns '5.1'
+
+    # Device properties
+    request.user_agent.device  # returns Device(family='iPhone')
+    request.user_agent.device.family  # returns 'iPhone'
